9ffc788ad4bcee31997bea321323eba37ca40805
Shekhar <shekharsuman689@gmail.com>
2024-07-30 10:47:01 +0530

    change

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -3,9 +3,7 @@ import axios from "axios";
 
 export async function productsData(){
     const products = await axios.get(
-             "https://fakestoreapi.com/products"
-        
-    );
-        
-    return products
+    "https://fakestoreapi.com/products"
+   );       
+    return products;
 }
